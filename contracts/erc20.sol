4fd7b6251f4b8fc6996ed495daf7d66fbeba51a7
cactustune <ojoshegun42@gmail.com>
2022-10-21 00:27:28 +0100

    erc20 token deployment

---
+++ contracts/erc20.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Tune is ERC20 {
+    address public owner;
+    address payable [] public receievers;
+    uint256 months = 1697839280;
+    uint start = block.timestamp;
+    
+    constructor() public ERC20("Tune", "XYZ") {
+        _mint(msg.sender, 100000000 * 10 **18);
+        owner == msg.sender;
+    }
+
+    function calculate(address payable [] memory _address) public {
+        for(uint i =0 ; i<= _address.length; i++){
+            receievers.push(_address[i]);
+        }
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == owner, "only owner can mint token");
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    
+    receive() payable external {
+        require(start <= months , "Dispersing period expired");
+        require(msg.value > 0, "Not enough ether");
+        uint256 disperse = msg.value / 10;
+
+        for(uint i =0; i <= 10; i++){
+        receievers[i].transfer(disperse);
+    }
+    }
+}
